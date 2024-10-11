accc9d9fabb6599068319f5a131f269192c817da
juliemeta <juliemonefeldt_404@hotmail.com>
2024-10-11 09:09:17 +0200

    Added New York to list view upon page load

--- javascript/index.js
+++ javascript/index.js
@@ -10,6 +10,17 @@ function updateTimeZone() {
     "h:mm [<small>]A[</small>]"
   );
 
+  let newYorkTime = moment().tz("America/New_York");
+
+  let newYorkElement = document.querySelector("#new_york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY, dddd");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm [<small>]A[</small>]"
+  );
+
   let tokyoTime = moment().tz("Asia/Tokyo");
 
   let tokyoElement = document.querySelector("#tokyo");
